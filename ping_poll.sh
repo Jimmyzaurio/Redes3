68c001ffe91cb0af2b783bcbe796a553debc4fc0
Jimmyzaurio <jsp.saucedo@gmail.com>
2018-04-04 13:11:16 -0500

    ping poller if

--- ping_poll.sh
+++ ping_poll.sh
@@ -6,7 +6,12 @@ DIREC[2]="8.12.0.116"
 
 for IP in "${DIREC[@]}"
 do
-  sudo ping $IP -c 1
+  #sudo ping $IP -c 1
   packet_loss=$(sudo ping -c 1 -q $IP | grep -oP '\d+(?=% packet loss)')
+  echo "$IP % de perdida: $packet_loss"
+
   # SWAKS to admin mail 
+  if [ "$packet_loss" -ge 40 ]; then
+    echo -e "\tEnviando email de ALERTA"
+  fi
 done
